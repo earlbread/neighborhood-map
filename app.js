d4c15de274e81efc52f107bf40b33b600ea58d7e
Seunghun Lee <waydi1@gmail.com>
2016-11-27 22:13:56 -0800

    Make an array of locations

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
+var markers = [];
+var infoWindows = [];
+
 function initMap() {
-  var metrotown = {lat: 49.227626, lng: -123.007576};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: metrotown,
-    disableDefaultUI: true
-  });
-  var marker = new google.maps.Marker({
-    position: metrotown,
-    map: map
-  });
+    var metrotown = {lat: 49.227626, lng: -123.007576};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: metrotown,
+        disableDefaultUI: true
+    });
+
+    var locations = [
+        {
+            title: 'Metrotown Public Library',
+            location: {lat: 49.228365, lng: -123.006709},
+            description: 'Metrotown Public Library'
+        },
+        {
+            title: 'Metropolis at Metrotown',
+            location: {lat: 49.226771, lng: -123.001318},
+            description: 'The biggest mall in burnaby'
+        }
+    ];
+
+    locations.forEach(function(loc) {
+        var marker = new google.maps.Marker({
+            position: loc.location,
+            map: map,
+            title: loc.title
+        });
+
+        var infoWindow = new google.maps.InfoWindow({
+            content: loc.description
+        });
+
+        marker.addListener('click', function() {
+            infoWindow.open(map, marker);
+        });
+
+        markers.push(marker);
+        infoWindows.push(infoWindow);
+    });
 }
