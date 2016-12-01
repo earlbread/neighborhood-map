c437ed0609264eff2c5c0d42e1dbd0a03d3fc793
Seunghun Lee <waydi1@gmail.com>
2016-11-30 23:05:59 -0800

    Refactor app.js

--- js/app.js
+++ js/app.js
@@ -1,77 +1,81 @@
-var ViewModel = function() {
-    this.searchText = ko.observable('');
-    var markers = [];
-    var infoWindows = [];
+var locations = [
+    {
+        title: 'Metropolis at Metrotown',
+        location: {lat: 49.226771, lng: -123.001318},
+        description: 'The biggest mall in burnaby'
+    },
+    {
+        title: 'Metrotown Public Library',
+        location: {lat: 49.228365, lng: -123.006709},
+        description: 'Metrotown Public Library'
+    }
+];
 
-    function initMap() {
-        var metrotown = {lat: 49.227626, lng: -123.007576};
+var ViewModel = function() {
+    var self = this;
+    this.searchText = ko.observable('');
+    this.map = null;
+
+
+    var markers = [];
+    var largeInfoWindow = new google.maps.InfoWindow();
+
+    function initMap(center) {
         var map = new google.maps.Map(document.getElementById('map'), {
             zoom: 15,
-            center: metrotown,
+            center: center,
             disableDefaultUI: true
         });
 
-        var locations = [
-            {
-                title: 'Metrotown Public Library',
-                location: {lat: 49.228365, lng: -123.006709},
-                description: 'Metrotown Public Library'
-            },
-            {
-                title: 'Metropolis at Metrotown',
-                location: {lat: 49.226771, lng: -123.001318},
-                description: 'The biggest mall in burnaby'
-            }
-        ];
-
-        var largeInfoWindow = new google.maps.InfoWindow();
-
-        for (var i = 0; i < locations.length; i++) {
-            var loc = locations[i];
-            var marker = new google.maps.Marker({
-                position: loc.location,
-                title: loc.title,
-                animation: google.maps.Animation.DROP,
-                id: i
-            });
-
-            marker.addListener('click', function() {
-                populateInfoWindow(this, largeInfoWindow);
-            });
-
-            markers.push(marker);
-        }
-
-        function ShowListings() {
-            var bounds = new google.maps.LatLngBounds();
-
-            for (var i = 0; i < markers.length; i++) {
-                markers[i].setMap(map);
-                bounds.extend(markers[i].position);
-            }
-            map.fitBounds(bounds);
-        }
-
-        function HideListings() {
-            for (var i = 0; i < markers.length; i++) {
-                markers[i].setMap(null);
-            }
-        }
-
-        function populateInfoWindow(marker, infoWindow) {
-            if(infoWindow.marker != marker) {
-                infoWindow.marker = marker;
-                infoWindow.setContent('<div>' + marker.title + '</div>');
-                infoWindow.open(map, marker);
-
-                infoWindow.addListener('closeclick', function() {
-                    infoWindow.setMarker(null);
-                });
-            }
-        }
+        return map;
     }
 
-    initMap();
+    for (var i = 0; i < locations.length; i++) {
+        var loc = locations[i];
+        var marker = new google.maps.Marker({
+            position: loc.location,
+            title: loc.title,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+
+        marker.addListener('click', function() {
+            populateInfoWindow(self.map, this, largeInfoWindow);
+        });
+
+        markers.push(marker);
+    }
+
+    var showListings = function(map) {
+        var bounds = new google.maps.LatLngBounds();
+
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+            bounds.extend(markers[i].position);
+        }
+        map.fitBounds(bounds);
+    };
+
+    var hideListings = function() {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(null);
+        }
+    };
+
+    var populateInfoWindow = function(map, marker, infoWindow) {
+        if(infoWindow.marker != marker) {
+            infoWindow.marker = marker;
+            infoWindow.setContent('<div>' + marker.title + '</div>');
+            infoWindow.open(map, marker);
+
+            infoWindow.addListener('closeclick', function() {
+                infoWindow.marker = null;
+            });
+        }
+    };
+
+    this.map = initMap(locations[0].location);
+    showListings(this.map);
 };
 
 function initApp() {
