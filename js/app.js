06279c132afb2e6a5989de2ec7ce11e42967f9ab
Seunghun Lee <waydi1@gmail.com>
2016-12-01 17:01:09 -0800

    Make search and refactor

--- js/app.js
+++ js/app.js
@@ -1,85 +1,98 @@
 var locations = [
     {
         title: 'Metropolis at Metrotown',
-        location: {lat: 49.226771, lng: -123.001318},
+        position: {lat: 49.226771, lng: -123.001318},
         description: 'The biggest mall in burnaby'
     },
     {
         title: 'Metrotown Public Library',
-        location: {lat: 49.228365, lng: -123.006709},
+        position: {lat: 49.228365, lng: -123.006709},
         description: 'Metrotown Public Library'
     }
 ];
 
-var ViewModel = function() {
-    var self = this;
-    this.searchText = ko.observable('');
-    this.map = null;
+function initMap(center) {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: center,
+        disableDefaultUI: true
+    });
 
+    return map;
+}
 
-    var markers = [];
-    var largeInfoWindow = new google.maps.InfoWindow();
+function populateInfoWindow(map, marker, infoWindow) {
+    if(infoWindow.marker != marker) {
+        infoWindow.marker = marker;
+        infoWindow.setContent('<div>' + marker.title + '</div>');
+        infoWindow.open(map, marker);
 
-    function initMap(center) {
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 15,
-            center: center,
-            disableDefaultUI: true
+        infoWindow.addListener('closeclick', function() {
+            infoWindow.marker = null;
         });
-
-        return map;
     }
+};
 
+function initMapInfo(map, markers, largeInfoWindow) {
     for (var i = 0; i < locations.length; i++) {
         var loc = locations[i];
         var marker = new google.maps.Marker({
-            position: loc.location,
+            position: loc.position,
             title: loc.title,
-            animation: google.maps.Animation.DROP,
             id: i
         });
 
         marker.addListener('click', function() {
-            populateInfoWindow(self.map, this, largeInfoWindow);
+            populateInfoWindow(map, this, largeInfoWindow);
         });
+
+        marker.infoWindow = populateInfoWindow;
 
         markers.push(marker);
     }
+}
 
-    var showListings = function(map) {
-        var bounds = new google.maps.LatLngBounds();
+var ViewModel = function(map, markers, infoWindow) {
+    var self = this;
 
+    self.searchText = ko.observable('');
+
+    self.searchedLocations = ko.computed(function() {
         for (var i = 0; i < markers.length; i++) {
-            markers[i].setMap(map);
-            bounds.extend(markers[i].position);
+            var marker = markers[i];
+
+
+            if (marker.title.toLowerCase().includes(self.searchText().toLowerCase())) {
+                marker.setMap(map);
+            } else {
+                marker.setMap(null);
+            }
         }
-        map.fitBounds(bounds);
-    };
 
-    var hideListings = function() {
-        for (var i = 0; i < markers.length; i++) {
-            markers[i].setMap(null);
-        }
-    };
+        return markers.filter(function(marker) {
+            return marker.map !== null;
+        });
+    });
 
-    var populateInfoWindow = function(map, marker, infoWindow) {
-        if(infoWindow.marker != marker) {
-            infoWindow.marker = marker;
-            infoWindow.setContent('<div>' + marker.title + '</div>');
-            infoWindow.open(map, marker);
+    self.removeSearchText = function() {
+        self.searchText('');
+    }
 
-            infoWindow.addListener('closeclick', function() {
-                infoWindow.marker = null;
-            });
-        }
-    };
-
-    this.map = initMap(locations[0].location);
-    showListings(this.map);
+    self.populateInfoWindow = function(marker) {
+        self.searchText('');
+        marker.infoWindow(map, marker, infoWindow);
+    }
 };
 
 function initApp() {
-    ko.applyBindings(new ViewModel());
+    var map  = null;
+    var markers = [];
+    var largeInfoWindow = new google.maps.InfoWindow();
+
+    map = initMap(locations[0].position);
+    initMapInfo(map, markers, largeInfoWindow);
+
+    ko.applyBindings(new ViewModel(map, markers, largeInfoWindow));
 }
 
 function googleError() {
